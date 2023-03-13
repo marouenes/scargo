949ac94eba0100c63bf9043412d5c20f6248afc6
Patryk Parylo <ppa@spyro-soft.com>
2023-03-13 09:48:11 +0100

    Rework UT for scargo test command. Add UT for fail scenario.

--- tests/ut/ut_scargo_test.py
+++ tests/ut/ut_scargo_test.py
@@ -1,36 +1,236 @@
-import shutil
 from pathlib import Path
 
 import pytest
 from pytest_subprocess import FakeProcess
 
-from scargo.commands.test import scargo_test
+from scargo.commands.test import run_it, run_ut, scargo_test
 from scargo.config import Config
-from scargo.path_utils import get_project_root
+from tests.ut.utils import get_test_project_config
+
+PROJECT_ROOT_PATH = Path("root_path")
+TEST_SRC_DIR = Path(PROJECT_ROOT_PATH, "tests")
+TEST_BUILD_DIR = Path(PROJECT_ROOT_PATH, "build", "tests")
+GCOV_EXECUTABLE = "gcov_executable_example"
 
 
-def test_scargo_test_no_test_dir(create_new_project: None) -> None:
-    shutil.rmtree("tests")
-    with pytest.raises(SystemExit):
-        scargo_test(False)
+@pytest.fixture
+def mock_get_project_root(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(
+        "scargo.commands.test.get_project_root", lambda: PROJECT_ROOT_PATH
+    )
 
 
-def test_scargo_test_no_cmake_file(
-    create_new_project: None, caplog: pytest.LogCaptureFixture, config: Config
+@pytest.fixture
+def config(monkeypatch: pytest.MonkeyPatch) -> Config:
+    test_project_config = get_test_project_config()
+
+    test_project_config.tests.gcov_executable = GCOV_EXECUTABLE
+
+    monkeypatch.setattr(
+        "scargo.commands.test.prepare_config",
+        lambda: test_project_config,
+    )
+
+    return test_project_config
+
+
+def test_scargo_test(
+    config: Config,
+    mock_get_project_root: None,
+    fp: FakeProcess,
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    Path("tests/CMakeLists.txt").unlink()
-    with pytest.raises(SystemExit):
+    # ARRANGE
+    monkeypatch.setattr(Path, "exists", lambda _: True)
+    monkeypatch.setattr("scargo.commands.test.run_ut", lambda *_: None)
+
+    conan_install_cmd = [
+        "conan",
+        "install",
+        str(TEST_SRC_DIR),
+        "-if",
+        str(TEST_BUILD_DIR),
+    ]
+    conan_build_cmd = ["conan", "build", str(TEST_SRC_DIR), "-bf", str(TEST_BUILD_DIR)]
+
+    fp.register(conan_install_cmd)
+    fp.register(conan_build_cmd)
+
+    # ACT
+    scargo_test(False)
+
+    # ASSERT
+    assert fp.calls[0] == conan_install_cmd
+    assert fp.calls[1] == conan_build_cmd
+    assert len(fp.calls) == 2
+
+
+def test_scargo_test_run_ut_verbose(
+    config: Config,
+    mock_get_project_root: None,
+    fp: FakeProcess,
+) -> None:
+    # ARRANGE
+    ctest_cmd = ["ctest", "--verbose"]
+    html_covarage_cmd = [
+        "gcovr",
+        "-r",
+        "ut",
+        ".",
+        "--html",
+        "ut-coverage.html",
+        "--gcov-executable",
+        GCOV_EXECUTABLE,
+    ]
+
+    fp.register(ctest_cmd)
+    fp.register(html_covarage_cmd)
+
+    # ACT
+    run_ut(config, True, TEST_BUILD_DIR)
+
+    # ASSERT
+    assert fp.calls[0] == ctest_cmd
+    assert fp.calls[1] == html_covarage_cmd
+    assert len(fp.calls) == 2
+
+
+def test_scargo_test_run_it_verbose(
+    config: Config,
+    mock_get_project_root: None,
+    fp: FakeProcess,
+) -> None:
+    # ARRANGE
+    ctest_cmd = ["ctest", "--verbose"]
+    html_covarage_cmd = [
+        "gcovr",
+        "-r",
+        "it",
+        ".",
+        "--txt",
+        "it-coverage.txt",
+        "--html",
+        "it-coverage.html",
+        "--gcov-executable",
+        GCOV_EXECUTABLE,
+    ]
+    cat_cmd = "cat it-coverage.txt"
+
+    fp.register(ctest_cmd)
+    fp.register(html_covarage_cmd)
+    fp.register(cat_cmd)
+
+    # ACT
+    run_it(config, True)
+
+    # ASSERT
+    assert fp.calls[0] == ctest_cmd
+    assert fp.calls[1] == html_covarage_cmd
+    assert fp.calls[2] == cat_cmd
+    assert len(fp.calls) == 3
+
+
+def test_no_test_dir_fail(
+    config: Config,
+    mock_get_project_root: None,
+    caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    # ARRANGE
+    monkeypatch.setattr(Path, "exists", lambda _: False)
+
+    # ACT
+    with pytest.raises(SystemExit) as error:
         scargo_test(False)
 
+    # ASSERT
+    assert "Directory `tests` does not exist." in caplog.text
+    assert error.value.code == 1
 
-def test_scargo_test(create_new_project: None, fp: FakeProcess) -> None:
-    project_root = get_project_root()
-    tests_src_dir = project_root / "tests"
-    test_build_dir = project_root / "build/tests"
-    html_coverage_file = "ut-coverage.html"
 
-    fp.register(f"conan install {tests_src_dir} -if {test_build_dir}")
-    fp.register(f"conan build {tests_src_dir} -bf {test_build_dir}")
-    fp.register("ctest")
-    fp.register(f"gcovr -r ut . --html {html_coverage_file}")
-    scargo_test(False)
+# def test_no_tests_cmake_file_fail(
+#     create_new_project: None, caplog: pytest.LogCaptureFixture, config: Config
+# ) -> None:
+#     Path("tests/CMakeLists.txt").unlink()
+#     with pytest.raises(SystemExit):
+#         scargo_test(False)
+
+
+def test_conan_tests_install_fail(
+    config: Config,
+    mock_get_project_root: None,
+    fp: FakeProcess,
+    caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    # ARRANGE
+    monkeypatch.setattr(Path, "exists", lambda _: True)
+
+    fp.register(
+        ["conan", "install", str(TEST_SRC_DIR), "-if", str(TEST_BUILD_DIR)],
+        returncode=1,
+    )
+    fp.register(["conan", "build", str(TEST_SRC_DIR), "-bf", str(TEST_BUILD_DIR)])
+
+    # ACT
+    with pytest.raises(SystemExit) as error:
+        scargo_test(False)
+
+    # ASSERT
+    assert "Failed to build tests." in caplog.text
+    assert error.value.code == 1
+
+
+def test_conan_tests_build_fail(
+    config: Config,
+    mock_get_project_root: None,
+    fp: FakeProcess,
+    caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    # ARRANGE
+    monkeypatch.setattr(Path, "exists", lambda _: True)
+
+    fp.register(["conan", "install", str(TEST_SRC_DIR), "-if", str(TEST_BUILD_DIR)])
+    fp.register(
+        ["conan", "build", str(TEST_SRC_DIR), "-bf", str(TEST_BUILD_DIR)], returncode=1
+    )
+
+    # ACT
+    with pytest.raises(SystemExit) as error:
+        scargo_test(False)
+
+    # ASSERT
+    assert "Failed to build tests." in caplog.text
+    assert error.value.code == 1
+
+
+def test_scargo_test_run_ut_fail(
+    config: Config,
+    mock_get_project_root: None,
+    caplog: pytest.LogCaptureFixture,
+    fp: FakeProcess,
+) -> None:
+    # ARRANGE
+    fp.register(["ctest"])
+    fp.register(
+        [
+            "gcovr",
+            "-r",
+            "ut",
+            ".",
+            "--html",
+            "ut-coverage.html",
+            "--gcov-executable",
+            GCOV_EXECUTABLE,
+        ],
+        returncode=1,
+    )
+
+    # ACT
+    with pytest.raises(SystemExit) as error:
+        run_ut(config, False, TEST_BUILD_DIR)
+
+    # ASSERT
+    assert "Fail to run project tests" in caplog.text
+    assert error.value.code == 1
